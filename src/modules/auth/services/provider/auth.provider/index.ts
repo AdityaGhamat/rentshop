584125d06bcc8d090dd2549a30e7ae67ec3f6047
AdityaGhamat <adityaghamat01@gmail.com>
2025-12-14 18:32:29 +0530

    feat:added controller in auth module.

--- src/modules/auth/services/provider/auth.provider/index.ts
+++ src/modules/auth/services/provider/auth.provider/index.ts
@@ -1,5 +1,6 @@
 export * from "./registerwithemail.provider";
 export * from "./registerwithphone.provider";
 export * from "./verifyphoneotpandcompleteregister.provider";
+export * from "./verifyphoneotpandlogin.provider";
 export * from "./loginwithemail.provider";
 export * from "./loginwithphone.provider";
